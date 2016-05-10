675c8c03bf35fddfe3a704619832b280981d9764
xs <xushuo@chinaskycloud.com>
2016-05-10 16:25:38 +0800

    添加 karma和jasmine插件。进行单元测试。

---
+++ js/add.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hadoop on 2016/5/10.
+ */
+function add (a, b) {
+    return a + b;
+}
---
+++ test/add-test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hadoop on 2016/5/10.
+ */
+describe("测试 某一组的方法",function(){
+    it("测试本组中add方法1",function(){
+        expect(add(1,2)).toEqual(3);
+    });
+});
